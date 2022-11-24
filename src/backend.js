4c006f147969e3f82ca1473d4fa38de1fa0b29d5
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2022-11-24 08:36:00 +0300

    feat: add functional to project

---
+++ src/backend.js
@@ -0,0 +1,4 @@
+export const API = 'http://localhost:5000'
+export const APInews = 'http://localhost:5000/posts'
+export const APItenders = 'http://localhost:5000/tenders'
+export const APIpost = 'http://localhost:5000/page'
