5410327e8134e83a2f79a4b9cfe30d9ce4d5f518
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2023-10-27 08:41:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router';
 import { useCookies } from 'react-cookie';
 export default function App() {
   const navigate = useNavigate();
-  const [cookies] = useCookies(['CookieConsent']);
+  const [cookies, setCookie, removeCookie] = useCookies(['CookieConsent']);
   return (
     <div>
       <Routers />
@@ -16,15 +16,34 @@ export default function App() {
             backgroundColor: 'rgba(14,78,129,1)',
             width: '100%',
             margin: '0 auto',
-            justifyContent: 'left',
+            justifyContent: 'center',
           }}
-          contentStyle={{ flex: 'none', maxWidth: '1100px', width: '100%' }}
-          buttonStyle={{ background: '#fff', padding: '10px 30px', margin: '20px' }}
-          buttonText={'Ок'}
-          debug={false}
+          contentStyle={{ maxWidth: '1300px', width: '100%' }}
+          buttonStyle={{ display: 'none' }}
+          // buttonText={'Принять'}
+          debug={true}
         >
-          Этот сайт использует файлы cookies для более комфортной работы пользователя. Продолжая
-          просмотр страниц сайта, вы соглашаетесь с использованием файлов cookies
+          Для обеспечения удобства пользователей сайта используются cookies
+          <Button
+            onClick={() => {
+              setCookie('_ga', 'GA1.1.1461999740.1696242335', { path: '/' });
+              setCookie('googtrans', '/ru/ru', { path: '/' });
+              setCookie('CookieConsent', true, { path: '/' });
+            }}
+            className={'cookie-container'}
+          >
+            Принять
+          </Button>
+          <Button
+            onClick={() => {
+              removeCookie('_ga');
+              removeCookie('googtrans');
+              removeCookie('CookieConsent');
+            }}
+            className={'cookie-container'}
+          >
+            Отклонить
+          </Button>
           <Button onClick={() => navigate('/politika-cookie')} className={'cookie-container'}>
             Подробнее
           </Button>
