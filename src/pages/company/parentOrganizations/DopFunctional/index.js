751f6462f357ccb73b54820143a0f083b3e140d2
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2023-12-15 10:50:50 +0300

    fix

--- src/pages/company/parentOrganizations/DopFunctional/index.js
+++ src/pages/company/parentOrganizations/DopFunctional/index.js
@@ -7,6 +7,7 @@ export default function DopFunctional({ link, img, name, id, info }) {
         <a rel="noreferrer" target={'_blank'} href={link}>
           <img src={require(`../../../../assets/png/${img}.png`)} alt={name} />
         </a>
+        <h3 style={{ color: '#0d4475' }}>{name}</h3>
       </div>
       <div>{info}</div>
     </ContainerForInform>
