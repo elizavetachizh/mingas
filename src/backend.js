78bc77731a8bca352ac18cd979a0f947639e46d7
elizavetachizh <elizavetka.chizh@gmail.com>
2024-06-13 17:31:15 +0300

    changed styles for slider

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
-export const API = 'http://localhost:3000/admin'
- //  export const API = 'https://back.mingas.by/admin'
+// export const API = 'http://localhost:3000/admin'
+  export const API = 'https://back.mingas.by/admin'
   export const APIimage = 'https://back.mingas.by/public/images'
