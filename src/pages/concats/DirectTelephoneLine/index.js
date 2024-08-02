60a056182d94f880c3e5691f10ff8b5ec537fe1e
elizavetachizh <elizavetka.chizh@gmail.com>
2024-08-02 13:47:34 +0300

    added new map

--- src/pages/concats/DirectTelephoneLine/index.js
+++ src/pages/concats/DirectTelephoneLine/index.js
@@ -19,7 +19,7 @@ export default function PhoneLineServices() {
                   <tr>
                     <td>Контакт-центр УП «МИНГАЗ»</td>
                     <td>
-                      <a style={{ fontWeight: 'bold', fontSize:'22px' }} href={'tel:162'}>
+                      <a style={{ fontWeight: 'bold', fontSize: '22px' }} href={'tel:162'}>
                         162
                       </a>
                     </td>
@@ -27,18 +27,16 @@ export default function PhoneLineServices() {
                   <tr>
                     <td>График работы центра обслуживания клиентов</td>
                     <td>
-                      г. Минск, ул. П. Бровки, д. 1
-                      ежедневно с 8.00 до 20.00, кроме праздничных дней <br/>
-                      г. Заславль, ул. Советская, 79
-                      9.00-18.00 (пн-чт и 3-я суббота месяца), 9.00-16.45 (пт.), обед: 13.00-13.45
-
+                      г. Минск, ул. П. Бровки, д. 1 ежедневно с 8.00 до 20.00, кроме праздничных
+                      дней <br />
+                      г. Заславль, ул. Советская, 79 9.00-18.00 (пн-чт и 3-я суббота месяца),
+                      9.00-16.45 (пт.), обед: 13.00-13.45
                     </td>
                   </tr>
                   <tr>
                     <td>«Горячая линия»</td>
                     <td>
-                      <a href={'tel:+375(17)-299-28-70'}> +375(17)-299-28-70</a>,
-                      круглосуточно
+                      <a href={'tel:+375(17)-299-28-70'}> +375(17)-299-28-70</a>, круглосуточно
                     </td>
                   </tr>
                   <tr>
@@ -57,28 +55,18 @@ export default function PhoneLineServices() {
           <DivMap>
             <div style={{ position: 'relative', overflow: 'hidden' }}>
               <a
-                href="https://yandex.by/maps/org/mingaz/41119693302/?utm_medium=mapframe&utm_source=maps"
+                href="https://yandex.by/maps/org/tsentr_obsluzhivaniya_kliyentov_up_mingaz/51103648001/?utm_medium=mapframe&utm_source=maps"
                 style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}
               >
-                Мингаз
-              </a>
-              <a
-                href="https://yandex.by/maps/157/minsk/category/gas_supply_services/184108297/?utm_medium=mapframe&utm_source=maps"
-                style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}
-              >
-                Служба газового хозяйства в Минске
-              </a>
-              <a
-                href="https://yandex.by/maps/157/minsk/category/municipal_housing_authority/184108301/?utm_medium=mapframe&utm_source=maps"
-                style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '28px' }}
-              >
-                Коммунальная служба в Минске
+                Центр обслуживания клиентов УП Мингаз
               </a>
               <iframe
-                title={'карта'}
-                src="https://yandex.by/map-widget/v1/-/CCURMME-DA"
-                allowFullScreen={true}
+                src="https://yandex.by/map-widget/v1/?ll=27.593782%2C53.919477&mode=search&oid=51103648001&ol=biz&z=18.96"
+                width="560"
+                height="400"
+                allowFullScreen="true"
                 style={{ position: 'relative' }}
+                title={'карта'}
               />
             </div>
           </DivMap>
