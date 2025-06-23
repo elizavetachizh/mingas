b66d4743404c4e95713fb09d7b96b2167bc5bb86
elizavetachizh <elizavetka.chizh@gmail.com>
2025-06-23 14:39:05 +0300

    fix cookies files, fix posts

--- src/pages/PressCenter/SMI/schema.js
+++ src/pages/PressCenter/SMI/schema.js
@@ -1,12 +1,14 @@
 import { Article } from '../styles';
 
 export default function SchemaSMI({ href, src, content, date }) {
+  const [year, month, day] = date.split('-');
+  const changedDate = `${day}.${month}.${year}`;
   return (
     <Article>
       <a rel="noopener" target={'_blank'} href={href}>
         <img alt={''} src={`https://mingas.by/${src}`} />
         <div>
-          {date && <p style={{ color:'#575757' }}>{date}</p>}
+          {date && <p style={{ color: '#575757' }}>{changedDate}</p>}
           <p style={{ fontWeight: 'bold' }}>{content}</p>
         </div>
       </a>
